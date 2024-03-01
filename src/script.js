fefdd9460dd97d87c189a2cac19c78a9993ef1d6
loexperience <doubledollopped@gmaiil.com>
2024-03-01 13:56:09 +0000

    implemented basic tube geometry with bezier curves

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+
 
 /**
  * Base
@@ -9,14 +11,51 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0x4a4a4a) );
 
-// Test Cube
-const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const material = new THREE.MeshBasicMaterial()
-const boxMesh = new THREE.Mesh(geometry, material)
-scene.add(boxMesh)
+// Debug
+const gui = new GUI()
 
 
+// Test Branch
+
+const v1 = new THREE.Vector3(5, 0, 5);
+const v2 = new THREE.Vector3(-5, 0, -5);
+const vControl = new THREE.Vector3(0, 5, 0);
+const curve = new THREE.QuadraticBezierCurve3( v1, vControl, v2);
+const v3Array = curve.getPoints(20);
+const geometry = new THREE.BufferGeometry();
+geometry.setFromPoints(v3Array);
+const points = new THREE.Points(geometry, new THREE.PointsMaterial({color: 0xff0000, size: 0.25 }));
+scene.add(points);
+
+const tubeGeometry = new THREE.TubeGeometry(
+    curve,
+    20,
+    0.5,
+    8,
+    false
+)
+
+const tubeMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00})
+const tubeMesh = new THREE.Mesh(tubeGeometry, tubeMaterial)
+scene.add(tubeMesh)
+
+gui.add(vControl, 'y')
+    .min(0)
+    .max(10)
+    .step(0.1)
+    .name("Bezier Control")
+    .onFinishChange((value) => {
+        tubeMesh.geometry.dispose()
+        tubeMesh.geometry = new THREE.TubeGeometry(
+            curve,
+            20,
+            0.5,
+            8,
+            false
+        )
+    })
 
 // Sizes
 const sizes = {
