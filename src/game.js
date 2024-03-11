b5b568af1710dd7667ae27179c987a878c0a0613
loexperience <doubledollopped@gmaiil.com>
2024-03-11 11:57:37 +0000

    added title screen

--- src/game.js
+++ src/game.js
@@ -1,12 +1,27 @@
 import GameManager from './Classes/GameManger.js'
-import LSystem from './Classes/LSystem.js'
 import Start from './Classes/Start.js'
+
+
 
 
 /**
  * Base
  */
 const start = new Start()
+
+// Fake loading screen
+let loadingBar = document.getElementsByClassName('loading-bar')[0]
+loadingBar.style.transform =`scaleX(1)`
+
+loadingBar.addEventListener(
+    'transitionend', 
+    ()=>{
+        const titleDiv = document.getElementsByClassName('titlePage')[0]
+        titleDiv.style.display = 'none'
+        const body = document.body
+        body.style.visibility = 'visible'
+    }
+)
 
 // Game Loop
 const gameManager = new GameManager()
